a820a83656ca4c56374aad27ac5bb8e911ec6a05
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-11-01 19:29:10 -0400

    Started on a primes library.
    At this point it will use a parallel implementation of the Sieve
    of Eratosthenes

---
+++ libs/primes/primes.go
@@ -0,0 +1,35 @@
+package primes
+
+import . "../bitslice/bitslice"
+
+func Primes(limit uint) BitSlice {
+	length := limit / 8
+	if limit%8 > 0 {
+		length++
+	}
+
+	list := make([]byte, length)
+
+	// Initialize for values 2, 3, 5, and 7 already run.
+	// 0123456789 10 11 12 13 14 15 16 17...
+	// 0011010100  0  1  0  1  0  0  0  1
+	list[0] = 0x35
+	for i := 1; i < length; i += 3 {
+		list[i] = 0x14
+	}
+	for i := 2; i < length; i += 3 {
+		list[i] = 0x51
+	}
+	for i := 3; i < length; i += 3 {
+		list[i] = 0x45
+	}
+
+	// smallestGuaranteedPrime
+	// The largest number guaranteed to be prime at this point.
+	// This is based off the principle that in an iteration of the
+	//     sieve of eratosthenes for number I, all values less than
+	//     I^2 are final.
+	// Our array has been initialized for i = 7
+	var bigGPrime uint  = 49
+	var primes = Quick(list)
+}
