01780531dadbf802434c0541bad3f92d65efb9ca
Bobby Wertman <rwertman@polaris.cs.wcu.edu>
2011-11-01 20:07:13 -0400

    Boilerplate for result

---
+++ 89/roman.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+var numerals []string
+
+func main() {
+	data, _ := ioutil.ReadFile("roman.txt")
+	numerals = strings.Split(string(data), "\n")
+	total := 0
+	for _, numeral := range numerals {
+		total += len(numeral)
+		total -= len(findMinimalRepresentation(numeral))
+	}
+	fmt.Println(total)
+}
+
+func findMinimalRepresentation(s string) string {
+	return ""
+}
